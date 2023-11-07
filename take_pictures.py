36ad7d97f97e881a4385ba667b689494a97103b3
nojovo <github@nv.baumrasen.de>
2023-11-07 18:29:14 +0100

    added object selection

--- take_pictures.py
+++ take_pictures.py
@@ -1,8 +1,12 @@
-import numpy
+import numpy as np
 import cv2 as cv
 
 
 cap = cv.VideoCapture(0)
+
+lower_color = np.array([0, 0, 0])
+upper_color = np.array([200, 200, 200])
+
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -13,13 +17,26 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
+
+
+    rgb_frame = cv.cvtColor(frame, cv.COLOR_RGBA2RGB)
+    mask = cv.inRange(rgb_frame, lower_color, upper_color)
+    result = cv.bitwise_and(frame, frame, mask=mask)
+
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    x, y, w, h = cv.boundingRect(contours[0])
+    extracted_object = result[y:y + h, x:x + w]
+
+    resized_object = cv.resize(extracted_object, (1000, 1000))
+
+
+    cv.imshow('frame1', frame)
+    cv.imshow('frame2', result)
+    cv.imshow('frame3', resized_object)
+
     if cv.waitKey(1) == ord('q'):
         break
-# When everything done, release the capture
+
 cap.release()
 cv.destroyAllWindows()
 
