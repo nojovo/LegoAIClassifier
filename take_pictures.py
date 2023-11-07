6bae795a86472cbda56b2a976b377e154c218f94
nojovo <github@nv.baumrasen.de>
2023-11-07 16:32:56 +0100

    simple video player added

---
+++ take_pictures.py
@@ -0,0 +1,25 @@
+import numpy
+import cv2 as cv
+
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
