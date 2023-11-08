81a3b24ef1473e903e03f9802b8079ae80cc34f6
nojovo <github@nv.baumrasen.de>
2023-11-08 14:42:31 +0100

    simplified mask

--- take_pictures.py
+++ take_pictures.py
@@ -18,21 +18,12 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-
     rgb_frame = cv.cvtColor(frame, cv.COLOR_RGBA2RGB)
     mask = cv.inRange(rgb_frame, lower_color, upper_color)
     result = cv.bitwise_and(frame, frame, mask=mask)
 
-    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    x, y, w, h = cv.boundingRect(contours[0])
-    extracted_object = result[y:y + h, x:x + w]
-
-    resized_object = cv.resize(extracted_object, (1000, 1000))
-
-
     cv.imshow('frame1', frame)
     cv.imshow('frame2', result)
-    cv.imshow('frame3', resized_object)
 
     if cv.waitKey(1) == ord('q'):
         break
