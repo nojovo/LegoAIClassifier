6710c65d1df29c878b3268a828ba9494c6a29415
nojovo <github@nv.baumrasen.de>
2024-01-30 14:32:35 +0100

    added file to separate test data from training data

---
+++ separate_test_pictures.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import os
+import random
+import shutil
+
+test_image_number = 50
+
+parts = ["32316", "32140", "32270", "2780", "32073"]
+
+dirname = os.path.dirname(__file__)
+
+path_to_test_folder = os.path.join(dirname, "pictures/test/")
+path_to_train_folder = os.path.join(dirname, "pictures/train/")
+Path(path_to_test_folder).mkdir(parents=True, exist_ok=True)
+Path(path_to_train_folder).mkdir(parents=True, exist_ok=True)
+
+path_to_pictures = os.path.join(dirname, "pictures/edited_100/")
+
+
+for part_number in parts:
+    src_path = path_to_pictures + part_number
+    train_dst_path = path_to_train_folder + part_number
+    test_dst_path = path_to_test_folder + part_number
+
+    files = os.listdir(src_path)
+    for i in range(test_image_number):
+        Path(test_dst_path).mkdir(parents=True, exist_ok=True)
+
+        file = random.choice(files)
+        files.remove(file)
+        shutil.move(src_path + f"/{file}", test_dst_path + f"/{file}")
+
+    for file in files:
+        Path(train_dst_path).mkdir(parents=True, exist_ok=True)
+        shutil.move(src_path + f"/{file}", train_dst_path + f"/{file}")
